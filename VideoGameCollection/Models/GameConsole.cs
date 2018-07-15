0e10abc4075ee001d08ee8168ecb8db7e7f0a32a
BryanKlinglesmith <b.klinglesmith@yahoo.com>
2018-07-15 17:03:17 -0400

    added validation to the gametitle section on the _entries view page

--- VideoGameCollection/Models/GameConsole.cs
+++ VideoGameCollection/Models/GameConsole.cs
@@ -8,13 +8,14 @@ namespace VideoGameCollection.Models
 {
     public class GameConsole
     {
-        //This is just a list of Game consoles that can be used in the drop down list for creating a new game or editing an existing one
+        /// <summary>
+        /// This is just a list of Game consoles that I am using for a drop down list when creating a new game or editing an existing one
+        /// </summary>
 
         public static IEnumerable<SelectListItem> GetConsoleTypesList()
         {
             IList<SelectListItem> consoleTypes = new List<SelectListItem>
             {
-                new SelectListItem() {Text="--Select One--", Value="null"},
                 new SelectListItem() {Text="Playstation 4", Value="PS4"},
                 new SelectListItem() { Text="Xbox One", Value="XB1"},
                 new SelectListItem() { Text="Nintendo Switch", Value="SWITCH"},
--- VideoGameCollection/Models/VideoGame.cs
+++ VideoGameCollection/Models/VideoGame.cs
@@ -9,9 +9,24 @@ namespace VideoGameCollection.Models
     public class VideoGame
     {
         public int Id { get; set; }
+
+        /// <summary>
+        /// I am making gametitle a required field so that blank entries
+        /// aren't submitted to the database. I'm also limiting the 
+        /// max number of characters allowed for the game titles.
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
         public string GameTitle { get; set; }
+
         public string Console { get; set; }
 
+        /// <summary>
+        /// Favorite is a display template in the shared views folder.  
+        /// I'm using it to display either "Yes" or "No" instead of 
+        /// an image of checkbox with a check or an empty checkbox
+        /// on the Index view page
+        /// </summary>
         [UIHint("Favorite")]
         public bool Favorite { get; set; }
     }
